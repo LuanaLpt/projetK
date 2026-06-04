ad78d2087ce371e16a6eee3dec029309edbe15b8
Luana <luana.lpt@outlook.fr>
2026-06-04 16:52:10 +0200

    ajout de la page admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Art;
+use App\Form\ArtType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,10 +19,45 @@ final class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
-    #[Route('/admin/art/nouveau', name: 'app_admin_art_new')]
+    #[Route('/admin/art/nouveau', name: 'app_admin_art_new', methods: ['GET', 'POST'])]
     public function nouveau(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $art = new Art();
+        $form = $this->createForm(ArtType::class, $art);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mainImages = $form->get('mainImages')->getData();
+            $transiImages = $form->get('transiImages')->getData();
+            if($mainImages){
+                foreach ($mainImages as $mainImage) {
+                    $newFileName = uniqid() . '.' . $mainImage->guessExtension();
+                    $mainImage->move($this->getParameter('art_directory'), $newFileName);
 
-        return $this->render('admin/nouveau.html.twig', []);
+                    $mainImage = new \App\Entity\MainImage();
+                    $mainImage->setFilename($newFileName);
+                    $art->addMainImage($mainImage);
+                    $entityManager->persist($mainImage);
+                }
+            }
+            if($transiImages){
+                foreach ($transiImages as $transiImage) {
+                    $newFileName = uniqid() . '.' . $transiImage->guessExtension();
+                    $transiImage->move($this->getParameter('art_directory'), $newFileName);
+
+                    $transiImage = new \App\Entity\TransiImage();
+                    $transiImage->setFilename($newFileName);
+                    $art->addTransiImage($transiImage);
+                    $entityManager->persist($transiImage);
+                }
+            }
+
+            $entityManager->persist($art);
+            $entityManager->flush();
+            $this->addFlash('success', 'L\'œuvre a bien été publiée !');
+            return $this->redirectToRoute('app_admin_art_new');
+        }
+        return $this->render('admin/nouveau.html.twig', [
+            'artForm' => $form->createView(),
+        ]);
     }
 }
--- src/Form/ArtType.php
+++ src/Form/ArtType.php
@@ -6,7 +6,7 @@ use App\Entity\Art;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,7 +21,7 @@ class ArtType extends AbstractType
                 'label' => 'Titre',
                 'attr' => ['placeholder' => 'Titre du projet ou produit'],
             ])
-            ->add('Description', textareaType::class, [
+            ->add('description', textareaType::class, [
                 'label' => 'Description',
                 'attr' => ['placeholder' => 'Description du projet ou produit'],
             ])
@@ -32,13 +32,13 @@ class ArtType extends AbstractType
                 'Produit' => 'Produit',
             ],
             ])
-            ->add('price', NumberType::class, [
+            ->add('price', MoneyType::class, [
                 'label' => 'Prix',
                 'currency' => 'EUR',
                 'required' => false,
                 'attr' => ['placeholder' => 'Prix du produit'],
             ])
-            ->add('mainImage', FileType::class, [
+            ->add('mainImages', FileType::class, [
                 'label' => 'Ajouter photos',
                 'mapped' => false,
                 'multiple' => true,
